41309c6f21a7661c1ee79a00e453d734c64dd5ee
1244952898 <1244952898@qq.com>
2017-07-17 09:23:44 +0800

    extendentity

---
+++ mq.model.extendedentity/employeebg/ActiveFileDepartmentExtend.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using mq.model.dbentity;
+
+namespace mq.model.extendedentity.employeebg
+{
+    public class ActiveFileDepartmentExtend : V_BG_ActiveFile_Department
+    {
+        public int Count { get; set; }
+    }
+}
