23fc532ed4d5253da008757f95aa195db83cca51
1244952898 <1244952898@qq.com>
2017-07-17 14:01:42 +0800

    1

--- mq.test.im/Controllers/ChatController.cs
+++ mq.test.im/Controllers/ChatController.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using mq.application.service.Implement;
+using mq.application.service.Interface;
 
 namespace mq.test.im.Controllers
 {
     public class ChatController : Controller
     {
+        //IBgUserService _bgUserService=new BgUserService();
         // GET: Chat
         public ActionResult Users()
         {
