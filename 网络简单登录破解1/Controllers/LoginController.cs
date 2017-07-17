d4690606d5d23f6f16dd4921505ebc0f74671bf9
1244952898 <1244952898@qq.com>
2017-07-17 10:41:19 +0800

    破解

---
+++ 网络简单登录破解1/Controllers/LoginController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace 网络简单登录破解1.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(User user)
+        {
+            if (user.UserName == "11" && user.Password == "11")
+                return View(user);
+            else
+                return RedirectToAction("Login");
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+    public class User
+    {
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+    }
+}
---
+++ 网络简单登录破解1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="网络简单登录破解1.MvcApplication" Language="C#" %>
