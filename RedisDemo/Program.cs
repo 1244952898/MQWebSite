93dc3b81d5734384651f933ea7baec820a587b2f
1244952898 <1244952898@qq.com>
2017-07-17 10:39:13 +0800

    redisdemo

---
+++ RedisDemo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StackExchange.Redis;
+
+namespace RedisDemo
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            string key = "a";
+            string str = "aaaaaaa";
+            bool result = StackExchangeRedisHelper.StringSet(key, str);
+
+            string gtstr = StackExchangeRedisHelper.StringGet(key);
+            Console.WriteLine(gtstr);
+            Console.ReadLine();
+        }
+    }
+}
